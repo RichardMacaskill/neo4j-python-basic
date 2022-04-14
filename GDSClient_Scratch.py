85238b61ea2c09b5e0b4e5905df79bc22e928018
Richard Macaskill <richard.macaskill@neotechnology.com>
2022-04-14 16:03:06 +0100

    initial commit

--- GDSClient_Scratch.py
+++
@@ -1,5 +0,0 @@
-from graphdatascience import GraphDataScience
-
-# Use Neo4j URI and credentials according to your setup
-gds = GraphDataScience("bolt+s://cas-testlab-neo4j.co.uk:7687", auth=("neo4j", "Berlin99!"))
-gds.set_database('europeanroads')
